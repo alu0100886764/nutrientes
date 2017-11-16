95bfb889759a8b909767fc25c97ee8174bd57d78
Dani <alu0100886764@ull.edu.es>
2017-11-16 21:26:40 +0000

    Incluyendo el modulo enumerable e implementando el metodo each

--- valor_energetico/lib/valor_energetico/lista.rb
+++ valor_energetico/lib/valor_energetico/lista.rb
@@ -1,50 +1,30 @@
 Node= Struct.new(:value, :next, :prev)
 
+#Lista doblemente enlazada
 class Lista
     attr_accessor :inicio, :final, :size
     
+    include Enumerable
+    
+    #Nos permite enumerar la clase Lista
+      def each
+        i = @final
+        f = @size
+        k = 0
+        while (k < f) do
+            yield i.value
+            i = i.next
+            k = k+1
+        end
+      end
+  
     def initialize()
         @inicio= Node.new(nil,nil,nil)
         @final= Node.new(nil,nil,nil)
         @size = 0
     end
     
-    def extract_beg()
-        if(@size==0)
-           puts "No hay elementos"
-        elsif(@size==1)
-           sale = Node.new(@inicio.value,nil,nil)
-           @size -= 1
-           @inicio = Node.new(nil,nil,nil)
-           @final = Node.new(nil,nil,nil)
-           "#{sale.value}"
-        elsif(@size > 1)
-           @size -= 1
-           sale = Node.new(@inicio.value,nil,nil)
-           @inicio.prev.next = nil
-           @inicio = @inicio.prev
-           "#{sale.value}"
-        end
-    end
-    
-    def extract_end()
-        if(@size==0)
-           puts "No hay elementos"
-        elsif(@size == 1)
-           sale = Node.new(@final.value,nil,nil)
-           @size-=1
-           @inicio = Node.new(nil,nil,nil)
-           @final = Node.new(nil,nil,nil)
-           "#{sale.value}"
-        elsif(@size > 1)
-           @size-=1
-           sale = Node.new(@final.value,nil,nil)
-           @final.next.prev = nil
-           @final = @final.next
-           "#{sale.value}"
-        end
-    end
-    
+    #Inserta un nuevo valor por princio
     def insert_single(valor) #inicio
         nuevo= Node.new(valor,nil,@inicio)
         if (@size < 1)
@@ -64,18 +44,59 @@ class Lista
           @size += 1
         end
     end
-
-    def insert_multiple(nodos)
-        nodos.each do |element|
+    
+    #Extrae el nodo de la cabeza
+    def extract_beg()
+        if(@size==0)
+           puts "No hay elementos"
+        elsif(@size==1)
+           sale = Node.new(@inicio.value,nil,nil)
+           @size -= 1
+           @inicio = Node.new(nil,nil,nil)
+           @final = Node.new(nil,nil,nil)
+           "#{sale.value}"
+        elsif(@size > 1)
+           @size -= 1
+           sale = Node.new(@inicio.value,nil,nil)
+           @inicio.prev.next = nil
+           @inicio = @inicio.prev
+           "#{sale.value}"
+        end
+    end
+    
+    #Extrae el nodo de la cola
+    def extract_end()
+        if(@size==0)
+           puts "No hay elementos"
+        elsif(@size == 1)
+           sale = Node.new(@final.value,nil,nil)
+           @size-=1
+           @inicio = Node.new(nil,nil,nil)
+           @final = Node.new(nil,nil,nil)
+           "#{sale.value}"
+        elsif(@size > 1)
+           @size-=1
+           sale = Node.new(@final.value,nil,nil)
+           @final.next.prev = nil
+           @final = @final.next
+           "#{sale.value}"
+        end
+    end
+    
+    #Inserta multiples valores en la lista
+    def insert_multiple(array)
+        array.each do |element|
             insert_single(element)
         end
     end
-
+    
+    #Devuelve true si la lista esta vacía
     def empty()
-        if(@inicio == nil)
+        if(@inicio.value == nil)
             return true
         else
             return false
         end
     end
+    
 end
