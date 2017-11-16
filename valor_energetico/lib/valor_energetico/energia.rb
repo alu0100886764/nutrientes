464b59bc7aed8049bd67e26286b5c411e628ba6f
Dani <alu0100886764@ull.edu.es>
2017-11-16 21:25:24 +0000

    Incluyendo el modulo comparable e implementando el metodo <=>

--- valor_energetico/lib/valor_energetico/energia.rb
+++ valor_energetico/lib/valor_energetico/energia.rb
@@ -1,12 +1,27 @@
+#Esta clase nos permite almacenar alimentos y calcular su IG
 class Energia
     
     attr_accessor :alimento, :proteinas, :glucidos, :lipidos, :valor_ener
+    
+include Comparable
+ 
+    #Nos permite comparar objetos de Energia
+    def <=>(other) 
+        @proteinas <=> other.proteinas
+    end
+
     
     def initialize(a,b,c,d)
        
         @alimento, @proteinas, @glucidos, @lipidos, @valor_ener = a, b, c, d, []
     end
-
+    
+    
+    
+    def add(a,b,c,d)
+        initialize(a,b,c,d)
+    end
+    
     def to_s
 
         out = "\t\t   Proteínas\tGlúcidos\tLípidos\n"
@@ -18,78 +33,32 @@ class Energia
             out << "\t\t"
             out << @lipidos.to_s
             out << "\n"
-       
         out
-  
         
     end
     
-    def get_nombre
-        
-        out = "#{@alimento}"
-        out        
-    end
-
-    def get_proteinas
-
-        #out = "#{@proteinas}"
-        #out  
-        
-        return @proteinas
-    end
-    
-    def get_glucidos
-
-        return @glucidos 
-    end
-
-    def get_lipidos
-
-     return @lipidos
-     
-    end
-    
+    #Calcula y devuelve el valor energetico
     def get_valor_energetico
 
-        
-        
-        
-            
-            return ((@proteinas + @glucidos) * 4 + @lipidos * 9).round(1)
-           
-              
+        return ((@proteinas + @glucidos) * 4 + @lipidos * 9).round(1)
+
     end
-   
+    
 end
 
+#Clase hija de Energia, contiene un objeto Energia y el nombre de un grupo de alimentos
 class Grupo_alimentos < Energia
 
-    attr_accessor :producto, :pr
-    
-    
-    
+  
     def initialize(a,b,c,d,e)
         super(a,b,c,d)
         @nombre_grupo = e
     end
     
-   
+    def to_s
     
-    def add(a,b,c,d)
-        @pr =Node.new(super(a,b,c,d))
-        @producto = @producto.insert_single(pr)
         
     end
     
-      
-    def to_s
-    
-        out = super
-        out << " "
-        out << "#{@group}"
-        out
-    end
-    
 end
-
 
